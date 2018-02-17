49ce0e70c8460a953c9b29019c58720a0bd12572
monikaj98 <36564515+monikaj98@users.noreply.github.com>
2018-02-17 20:02:12 +0530

    Add files via upload

---
+++ entertain.py
@@ -0,0 +1,46 @@
+# stores details of movies and displays them on website.
+import fresh_tomatoes
+import media
+
+# Creating various instances for the class Movie.
+insidious = media.Movie("Insidious",
+                        "The story is about a couple and his son with ghosts.",
+                        "insi.jpg",
+                        "https://www.youtube.com/watch?v=Gmpn9_D8sZ0")
+
+
+annabelle = media.Movie("Annabelle",
+                        "A myterious doll named annabelle having some spirit.",
+                        "anna.jpg",
+                        "https://www.youtube.com/watch?v=yd27oUBi3-I")
+
+
+Now_you_see_me = media.Movie("Now you see me",
+                             "American thriller film having great magicians",
+                             "now.jpg",
+                             "https://www.youtube.com/watch?v=4OtM9j2lcUA")
+
+
+The_fault_in_our_stars = media.Movie(
+                                "The fault in our stars",
+                                "American romantic director film",
+                                "fault.jpg",
+                                "https://www.youtube.com/watch?v=9ItBvH5J6ss")
+
+Hunger_games = media.Movie(
+    "Hunger games",
+    "American post-apocalyptic science fiction action adventurous film",
+    "hun.jpg",
+    "https://www.youtube.com/watch?v=mfmrPu43DF8")
+Hangover = media.Movie("Hangover",
+                       "Comedy film",
+                       "hang.jpg",
+                       "https://www.youtube.com/watch?v=TZc39afdeXU")
+
+# Creating an array called movies and store the Movie objects in that list.
+movies = [
+            insidious, annabelle, Now_you_see_me,
+            The_fault_in_our_stars, Hunger_games, Hangover]
+
+# using the python program fresh tomatoes for opening our website.
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,15 @@
+class Movie():
+    __name__ = "Movie House"
+    # class for representing a movie.
+    # Attributes having movie title,storyline,posterimage,and trailer.
+
+    def __init__(
+                 self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+                    self.title = movie_title
+                    self.storyline = movie_storyline
+                    self.poster_image_url = poster_image
+                    self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
