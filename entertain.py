fe6fb1798625280fd6635af92ee0e8caeaf12eee
monikaj98 <36564515+monikaj98@users.noreply.github.com>
2018-02-18 09:46:05 +0530

    Update entertain.py

--- entertain.py
+++ entertain.py
@@ -5,36 +5,36 @@ import media
 # Creating various instances for the class Movie.
 insidious = media.Movie("Insidious",
                         "The story is about a couple and his son with ghosts.",
-                        "insi.jpg",
+                        "insidious.jpg",
                         "https://www.youtube.com/watch?v=Gmpn9_D8sZ0")
 
 
 annabelle = media.Movie("Annabelle",
                         "A myterious doll named annabelle having some spirit.",
-                        "anna.jpg",
+                        "annabelle.jpg",
                         "https://www.youtube.com/watch?v=yd27oUBi3-I")
 
 
 Now_you_see_me = media.Movie("Now you see me",
                              "American thriller film having great magicians",
-                             "now.jpg",
+                             "nowyouseeme.jpg",
                              "https://www.youtube.com/watch?v=4OtM9j2lcUA")
 
 
 The_fault_in_our_stars = media.Movie(
                                 "The fault in our stars",
                                 "American romantic director film",
-                                "fault.jpg",
+                                "fault in our stars.jpg",
                                 "https://www.youtube.com/watch?v=9ItBvH5J6ss")
 
 Hunger_games = media.Movie(
     "Hunger games",
     "American post-apocalyptic science fiction action adventurous film",
-    "hun.jpg",
+    "hunger.jpg",
     "https://www.youtube.com/watch?v=mfmrPu43DF8")
 Hangover = media.Movie("Hangover",
                        "Comedy film",
-                       "hang.jpg",
+                       "hangover.jpg",
                        "https://www.youtube.com/watch?v=TZc39afdeXU")
 
 # Creating an array called movies and store the Movie objects in that list.
